b087680786c10a4a7ed1047a7e0fdba5fdb1d145
aryan chauhan <aryanchauhan0412@gmail.com>
2026-03-24 11:55:49 +0530

    fix env

--- frontend/src/api/notesApi.js
+++ frontend/src/api/notesApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000/notes";
+const API = `${import.meta.env.VITE_API_URL}/notes`;
 
 const getAuthConfig = () => {
   const token = localStorage.getItem("token");
